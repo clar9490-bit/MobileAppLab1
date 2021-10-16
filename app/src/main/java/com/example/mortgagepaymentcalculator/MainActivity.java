42157b2a64c3aedcd94f66a7b75ec5304f633dd1
Clarissa <clarissa.branje@ontariotechu.net>
2021-10-15 22:50:07 -0400

    documentaion

--- app/src/main/java/com/example/mortgagepaymentcalculator/MainActivity.java
+++ app/src/main/java/com/example/mortgagepaymentcalculator/MainActivity.java
@@ -23,34 +23,50 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    //on the calculate click
     public void calcButtonClick (View view){
+
+    //for each input use edit text to get the input from user and save it to a double variable
     EditText mpaTxt = findViewById(R.id.mpaTxt);
     double mpaDouble = Double.parseDouble(mpaTxt.getText().toString());
 
-        System.out.println(mpaDouble);
+    //System.out.println(mpaDouble); system.out.println were used for the testing process of the app
+
     EditText irTxt = findViewById(R.id.irTxt);
     double irDouble = Double.parseDouble(irTxt.getText().toString());
-    irDouble= irDouble * 0.01;
-    System.out.println(irDouble);
+    irDouble= irDouble * 0.01; //convert intrest rate to a percentage value
+    //System.out.println(irDouble);
 
     EditText apTxt = findViewById(R.id.apTxt);
     double apDouble = Double.parseDouble(apTxt.getText().toString());
 
+    //for to get the yearly ap multiply monthly by 12
     double n = apDouble * 12;
 
+    //preform exponetial math for easier calculation
     double exp = (double) Math.pow((1+irDouble),n);
 
+    //main math calculation
     double total = (mpaDouble*(exp)*irDouble )/ (exp - 1 );
+
+    //format the answer
         DecimalFormat frmt = new DecimalFormat();
         frmt.setMaximumFractionDigits(2);
         frmt.format(total);
 
+    //save total as a new double
     Double d = new Double (frmt.format(total));
     System.out.println(total);
+
+    //test of output //original output before intent
     //TextView totalTxt = findViewById(R.id.editTextName);
     //totalTxt.setText(d.toString());
+
+    //create intent
     Intent myIntent = new Intent(MainActivity.this,myReceiver.class);
+    //set resulting total to the data for intent
     myIntent.setData(Uri.parse(d.toString()));
+    //begin the activity for the received page
     startActivity(myIntent);
 
     }
--- app/src/main/java/com/example/mortgagepaymentcalculator/myReceiver.java
+++ app/src/main/java/com/example/mortgagepaymentcalculator/myReceiver.java
@@ -7,14 +7,24 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class myReceiver extends AppCompatActivity {
-    TextView text;
+    TextView text; // save the text with a text view
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //setting content view of the new page /activity
         setContentView(R.layout.my_receiver);
+
+        // set the text into the text view id
         text = findViewById(R.id.Welcome);
+
+        //receive the Intent
         Intent myIntent = getIntent();
+
+        //save incoming result
         String receiveMessage = myIntent.getDataString();
+
+        //format the final result text
         text.setText("$ "+receiveMessage);
 
     }
